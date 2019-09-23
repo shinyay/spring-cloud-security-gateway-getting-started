7b80b16a344cdcc1ece3685624a6a58b89fed0db
shinyay <shinya.com@gmail.com>
2019-09-23 18:57:30 +0900

    Add TokenRelayGatewayFilterFactory autowired

--- src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityGatewayGsApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityGatewayGsApplication.kt
@@ -2,9 +2,10 @@ package io.pivotal.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory
 
 @SpringBootApplication
-class SpringCloudSecurityGatewayGsApplication
+class SpringCloudSecurityGatewayGsApplication(val filterFactory: TokenRelayGatewayFilterFactory)
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudSecurityGatewayGsApplication>(*args)
