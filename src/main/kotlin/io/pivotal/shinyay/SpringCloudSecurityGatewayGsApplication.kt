07eeeffe4d1fae4df9192d327a5581c42cd08222
shinyay <shinya.com@gmail.com>
2019-09-23 20:26:02 +0900

    Add gateway role to hide backgoround service which has endpoint as resource

--- src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityGatewayGsApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityGatewayGsApplication.kt
@@ -2,10 +2,22 @@ package io.pivotal.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory
 
 @SpringBootApplication
-class SpringCloudSecurityGatewayGsApplication(val filterFactory: TokenRelayGatewayFilterFactory)
+class SpringCloudSecurityGatewayGsApplication(val filterFactory: TokenRelayGatewayFilterFactory) {
+
+	fun routeLocator(builder: RouteLocatorBuilder): RouteLocator {
+		return builder.routes()
+				.route("resource") { r ->
+					r.path("/resource")
+							.filters { f -> f.filters(filterFactory.apply()) }
+							.uri("http://resource:9000")}
+				.build()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudSecurityGatewayGsApplication>(*args)
